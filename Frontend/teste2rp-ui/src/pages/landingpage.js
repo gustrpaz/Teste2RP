880ebe376136734f966089bf34eb3caed480036e
GUSTAVO PAZ <gustavo.paz@portalsesisp.org.br>
2022-06-27 22:36:50 -0300

    Inicio frontend

---
+++ Frontend/teste2rp-ui/src/pages/landingpage.js
@@ -0,0 +1,26 @@
+import { Component } from 'react';
+import { useState, useEffect } from 'react';
+import '../assets/css/landingpage.css';
+
+export default function LandingPage()  {
+
+  return (
+    <div className="fundo">
+      <div>aaaaaaaa</div>
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
